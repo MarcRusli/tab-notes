e9452c13e5bf1cbd206d0b06fdb468eb658e8ada
MarcRusli <97935580+MarcRusli@users.noreply.github.com>
2022-04-12 22:21:36 -0700

    got homefragment working

---
+++ app/src/main/java/com/amisuta/tabnotes/EditNoteFragment.kt
@@ -0,0 +1,42 @@
+package com.amisuta.tabnotes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [EditNoteFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class EditNoteFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment EditNoteFragment.
+         */
+        @JvmStatic
+        fun newInstance() = EditNoteFragment()
+
+    }
+}
--- app/src/main/java/com/amisuta/tabnotes/MainActivity.kt
+++ app/src/main/java/com/amisuta/tabnotes/MainActivity.kt
@@ -2,10 +2,27 @@ package com.amisuta.tabnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragment(HomeFragment.newInstance())
+    }
+
+    private fun replaceFragment(fragment: Fragment, isTransaction: Boolean = true) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        if (isTransaction) {
+            fragmentTransaction.setCustomAnimations(
+                android.R.anim.slide_out_right,
+                android.R.anim.slide_in_left
+            )
+        }
+
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+            .addToBackStack(fragment.javaClass.simpleName).commit()
     }
 }
